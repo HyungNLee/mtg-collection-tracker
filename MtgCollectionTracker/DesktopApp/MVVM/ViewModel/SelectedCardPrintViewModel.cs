c21a30d579fb72fd83f1271e8d0ce14529382038
Ryan (Hyung) Lee <hyungnaelee@gmail.com>
2022-02-16 18:35:20 -0800

    Create separate pages for different sections

---
+++ MtgCollectionTracker/DesktopApp/MVVM/ViewModel/SelectedCardPrintViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DesktopApp.MVVM.Model;
+
+using Prism.Mvvm;
+
+namespace DesktopApp.MVVM.ViewModel
+{
+    internal class SelectedCardPrintViewModel : BindableBase
+    {
+        /// <summary>
+        /// The selected card to show.
+        /// </summary>
+        private CardPrint _selectedCardPrint;
+        public CardPrint SelectedCardPrint
+        {
+            get { return _selectedCardPrint; }
+            set { SetProperty(ref _selectedCardPrint, value); }
+        }
+    }
+}
