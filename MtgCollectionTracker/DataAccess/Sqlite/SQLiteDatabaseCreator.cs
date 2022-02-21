9482c712e4e8452811a58a9401204f6105901a42
Ryan (Hyung) Lee <hyungnaelee@gmail.com>
2022-02-21 00:05:14 -0800

    Add SQLite nuget. Create SQLiteDatabaseCreator
    Relates to: #8

---
+++ MtgCollectionTracker/DataAccess/Sqlite/SQLiteDatabaseCreator.cs
@@ -0,0 +1,121 @@
+﻿using System.Data.SQLite;
+
+namespace DataAccess.Sqlite
+{
+    internal static class SQLiteDatabaseCreator
+    {
+        private const string _dbName = "MtgCollection.db";
+        private static readonly string _dbFilePath = $"{Path.Combine(Directory.GetCurrentDirectory(), _dbName)}";
+
+        /// <summary>
+        /// Returns the connection string to the SQLite database.
+        /// </summary>
+        public static string GetConnectionString = $"Data Source={_dbFilePath}";
+
+        /// <summary>
+        /// Creates the SQLite database if it doesn't exist.
+        /// </summary>
+        public static void CreateDatabaseIfNotExists()
+        {
+            if (!File.Exists(_dbFilePath))
+            {
+                SQLiteConnection.CreateFile(_dbFilePath);
+
+                CreateCardTable();
+                CreateSetTable();
+                CreateCardPrintTable();
+                CreateCollectionTable();
+                CreateOwnedCardTable();
+            }
+        }
+
+        private static void CreateCardTable()
+        {
+            using var db = new SQLiteConnection(GetConnectionString);
+            db.Open();
+            string sql = @"
+                    CREATE TABLE [Card] (
+                        Id    INTEGER NOT NULL UNIQUE,
+                        Name  TEXT NOT NULL UNIQUE,
+                        PRIMARY KEY(Id AUTOINCREMENT)
+                    );";
+
+            SQLiteCommand command = new(sql, db);
+            command.ExecuteNonQuery();
+        }
+
+        private static void CreateSetTable()
+        {
+            using var db = new SQLiteConnection(GetConnectionString);
+            db.Open();
+            string sql = @"
+                    CREATE TABLE [Set] (
+                        Id    INTEGER NOT NULL UNIQUE,
+                        Name  TEXT NOT NULL UNIQUE,
+                        PRIMARY KEY(Id AUTOINCREMENT)
+                    );";
+
+            SQLiteCommand command = new(sql, db);
+            command.ExecuteNonQuery();
+        }
+
+        private static void CreateCardPrintTable()
+        {
+            using var db = new SQLiteConnection(GetConnectionString);
+            db.Open();
+            string sql = @"
+                    CREATE TABLE CardPrint (
+                        Id	INTEGER NOT NULL UNIQUE,
+                        CardId	INTEGER NOT NULL,
+                        SetId	INTEGER NOT NULL,
+                        PictureUrl	TEXT,
+                        FlipPictureUrl	TEXT,
+                        UNIQUE(CardId,SetId),
+                        FOREIGN KEY(SetId) REFERENCES [Set](Id),
+                        FOREIGN KEY(CardId) REFERENCES Card(Id),
+                        PRIMARY KEY(Id AUTOINCREMENT)
+                    );";
+
+            SQLiteCommand command = new(sql, db);
+            command.ExecuteNonQuery();
+        }
+
+        private static void CreateCollectionTable()
+        {
+            using var db = new SQLiteConnection(GetConnectionString);
+            db.Open();
+            string sql = @"
+                    CREATE TABLE [Collection] (
+                        Id	INTEGER NOT NULL UNIQUE,
+                        Name	TEXT NOT NULL UNIQUE,
+                        IsDeck	INTEGER NOT NULL DEFAULT 0 CHECK(IsDeck = 0 OR IsDeck = 1),
+                        MainboardId	INTEGER,
+                        SideboardId	INTEGER,
+                        CHECK((IsDeck = 0 AND MainboardId IS null AND SideboardId IS null) OR (IsDeck = 1 AND ((MainboardId IS null AND SideboardId IS null) OR (MainboardId IS NOT null AND SideboardId IS null) OR (MainboardId IS null AND SideboardId IS NOT null)))),
+                        PRIMARY KEY(Id AUTOINCREMENT)
+                    );";
+
+            SQLiteCommand command = new(sql, db);
+            command.ExecuteNonQuery();
+        }
+
+        private static void CreateOwnedCardTable()
+        {
+            using var db = new SQLiteConnection(GetConnectionString);
+            db.Open();
+            string sql = @"
+                    CREATE TABLE OwnedCard (
+                        Id	INTEGER NOT NULL UNIQUE,
+                        CardPrintId	INTEGER NOT NULL,
+                        CollectionId	INTEGER NOT NULL,
+                        IsFoil	INTEGER NOT NULL DEFAULT 0 CHECK(IsFoil = 0 OR IsFoil = 1),
+                        PRIMARY KEY(Id AUTOINCREMENT),
+                        FOREIGN KEY(CardPrintId) REFERENCES CardPrint(Id),
+                        FOREIGN KEY(CollectionId) REFERENCES [Collection](Id)
+                    );";
+
+            SQLiteCommand command = new(sql, db);
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ MtgCollectionTracker/DataAccess/Sqlite/SqliteCardPrintService.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.SQLite;
+
+using DataAccess.Models;
+using DataAccess.Services;
+
+namespace DataAccess.Sqlite
+{
+    public class SqliteCardPrintService : ICardPrintService
+    {
+        private const string _dbName = "MtgCollection.db";
+
+        private string _dbFilePath = $"Filename={Path.Combine(Directory.GetCurrentDirectory(), _dbName)}";
+
+        public SqliteCardPrintService()
+        {
+            SQLiteDatabaseCreator.CreateDatabaseIfNotExists();
+        }
+
+        public Task<Card> GetCardAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CardPrintDetail> GetCardPrintDetailAsync(int cardId, int setId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CardPrintDetail>> GetCardPrintDetailsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Set> GetSetAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertCardAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertCardPrintAsync(int cardId, int setId, string pictureUrl, string flipPictureUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertSetAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
